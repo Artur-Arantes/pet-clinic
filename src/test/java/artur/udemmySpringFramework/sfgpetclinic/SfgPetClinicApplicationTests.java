ba1b5e6ebe6747049357d403b80e4e038d382757
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-11-16 13:41:58 -0300

    adding layout index page

--- src/test/java/artur/udemmySpringFramework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ src/test/java/artur/udemmySpringFramework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,13 @@
 package artur.udemmySpringFramework.sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
