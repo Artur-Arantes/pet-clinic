cab6fcc9c63450c975ba79660b733e5909f99b1d
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-11-16 14:34:41 -0300

    adding visit entity

--- pet-clinic-data/src/main/java/artur/springframework/petclinic/domain/Owner.java
+++ pet-clinic-data/src/main/java/artur/springframework/petclinic/domain/Owner.java
@@ -1,5 +1,11 @@
 package artur.springframework.petclinic.domain;
 
-public class Owner extends Person{
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class Owner extends Person{
+private Set<Pet> pets;
 }
---
+++ pet-clinic-data/src/main/java/artur/springframework/petclinic/domain/Visit.java
@@ -0,0 +1,13 @@
+package artur.springframework.petclinic.domain;
+
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Visit extends BaseEntity {
+  private LocalDate date;
+  private String description;
+  private Pet pet;
+}
