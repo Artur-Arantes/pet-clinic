83f283069ed1ab82eb136881ffc9240d994385b2
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-11-16 14:44:04 -0300

    adding speciality entity

--- pet-clinic-data/src/main/java/artur/springframework/petclinic/domain/Speciality.java
+++ pet-clinic-data/src/main/java/artur/springframework/petclinic/domain/Speciality.java
@@ -1,11 +1,10 @@
 package artur.springframework.petclinic.domain;
 
-import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Vet extends Person{
-  private Set<Speciality> speciality;
+public class Speciality extends BaseEntity{
+  private String description;
 }
