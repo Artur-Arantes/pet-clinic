ea784d18aa40502754cafa22179f065202e50a9f
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-11-18 10:20:54 -0300

    added specialty to vets in load data

--- pet-clinic-data/src/main/java/artur/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/artur/springframework/petclinic/bootstrap/DataLoader.java
@@ -3,9 +3,11 @@ package artur.springframework.petclinic.bootstrap;
 import artur.springframework.petclinic.domain.Owner;
 import artur.springframework.petclinic.domain.Pet;
 import artur.springframework.petclinic.domain.PetType;
+import artur.springframework.petclinic.domain.Speciality;
 import artur.springframework.petclinic.domain.Vet;
 import artur.springframework.petclinic.services.OwnerService;
 import artur.springframework.petclinic.services.PetTypeService;
+import artur.springframework.petclinic.services.SpecialitiesService;
 import artur.springframework.petclinic.services.VetServices;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
@@ -18,16 +20,36 @@ public class DataLoader implements CommandLineRunner {
   private final OwnerService ownerService;
   private final VetServices vetServices;
   private final PetTypeService petTypeService;
+  private final SpecialitiesService specialitiesService;
 
   @Override
   public void run(String... args) throws Exception {
-    PetType dog =new PetType();
+    int count= petTypeService.findAll().size();
+   if(count==0){
+     loadData();
+   }
+  }
+
+  private void loadData() {
+    PetType dog = new PetType();
     dog.setName("toto");
-    PetType dogSaved= petTypeService.save(dog);
+    PetType dogSaved = petTypeService.save(dog);
 
     PetType cat = new PetType();
     cat.setName("bolota");
-    PetType catSaved= petTypeService.save(cat);
+    PetType catSaved = petTypeService.save(cat);
+
+    Speciality radiology = new Speciality();
+    radiology.setDescription("Radiology");
+    Speciality savedRadiology=specialitiesService.save(radiology);
+
+    Speciality surgery = new Speciality();
+    surgery.setDescription("Surgery");
+    Speciality savedSurgery=specialitiesService.save(surgery);
+
+    Speciality dentistry = new Speciality();
+    dentistry.setDescription("Dentistry");
+    Speciality savedDentistry=specialitiesService.save(dentistry);
 
     Owner owner1 = new Owner();
     owner1.setId(1L);
@@ -37,7 +59,7 @@ public class DataLoader implements CommandLineRunner {
     owner1.setTelephone("15151");
     owner1.setLastName("campagnollo");
 
-    Pet misPet= new Pet();
+    Pet misPet = new Pet();
     misPet.setPetType(dogSaved);
     misPet.setOwner(owner1);
     misPet.setBirthDate(LocalDate.now());
@@ -53,7 +75,7 @@ public class DataLoader implements CommandLineRunner {
     owner2.setFirstName("toquio");
     owner2.setLastName("japan");
 
-    Pet toquiosPet= new Pet();
+    Pet toquiosPet = new Pet();
     toquiosPet.setPetType(cat);
     toquiosPet.setOwner(owner2);
     toquiosPet.setBirthDate(LocalDate.now());
@@ -68,6 +90,7 @@ public class DataLoader implements CommandLineRunner {
     vet1.setFirstName("joaquim");
     vet1.setId(1L);
     vet1.setLastName("Pereira");
+    vet1.getSpeciality().add(savedRadiology);
 
     vetServices.save(vet1);
 
@@ -75,6 +98,7 @@ public class DataLoader implements CommandLineRunner {
     vet2.setId(2L);
     vet2.setLastName("Bueno");
     vet2.setFirstName("Galvao");
+    vet2.getSpeciality().add(surgery);
 
     vetServices.save(vet2);
 
--- pet-clinic-data/src/main/java/artur/springframework/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/artur/springframework/petclinic/services/map/VetServiceMap.java
@@ -1,12 +1,19 @@
 package artur.springframework.petclinic.services.map;
 
+import artur.springframework.petclinic.domain.Speciality;
 import artur.springframework.petclinic.domain.Vet;
+import artur.springframework.petclinic.services.SpecialitiesService;
 import artur.springframework.petclinic.services.VetServices;
 import java.util.Set;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetServices {
+
+  private final SpecialitiesService specialitiesService;
+
   @Override
   public Set<Vet> findAll() {
     return super.findAll();
@@ -25,6 +32,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
   @Override
   public Vet save(Vet object) {
+    if (object.getSpeciality().size() > 0) {
+      object.getSpeciality().forEach(speciality -> {
+        if (speciality.getId() == null) {
+          Speciality savedSpeciality = specialitiesService.save(speciality);
+          speciality.setId(savedSpeciality.getId());
+        }
+      });
+    }
     return super.save(object);
   }
 
