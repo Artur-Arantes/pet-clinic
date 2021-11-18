7f42d4f67ea319f54328286cd1fde185da8f375f
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-11-18 14:01:27 -0300

    implemented vet service jpa repository

---
+++ pet-clinic-data/src/main/java/artur/springframework/petclinic/services/springdatajpa/VetSdJpaService.java
@@ -0,0 +1,50 @@
+package artur.springframework.petclinic.services.springdatajpa;
+
+import artur.springframework.petclinic.domain.Vet;
+import artur.springframework.petclinic.repositories.VetRepository;
+import artur.springframework.petclinic.services.VetServices;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("sprindatajpa")
+@RequiredArgsConstructor
+public class VetSdJpaService implements VetServices {
+
+  private final VetRepository vetRepository;
+
+  @Override
+  public Set<Vet> findAll() {
+    Set<Vet> vets = new HashSet<>();
+    vetRepository.findAll().forEach(vets::add);
+    return vets;
+  }
+
+  @Override
+  public Vet findById(Long aLong) {
+    return vetRepository.findById(aLong).orElse(null);
+  }
+
+  @Override
+  public Vet save(Vet object) {
+    return vetRepository.save(object);
+  }
+
+  @Override
+  public void delete(Vet object) {
+    vetRepository.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long aLong) {
+    vetRepository.deleteById(aLong);
+  }
+
+  @Override
+  public Vet findByLastName(String lastname) {
+    return null;
+  }
+}
