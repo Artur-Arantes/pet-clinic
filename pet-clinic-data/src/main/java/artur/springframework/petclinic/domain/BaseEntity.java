01df8b557844ffb4b5ea8d134d8659481c6bffa4
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-11-18 12:41:56 -0300

    uptade base entity

--- pet-clinic-data/src/main/java/artur/springframework/petclinic/domain/BaseEntity.java
+++ pet-clinic-data/src/main/java/artur/springframework/petclinic/domain/BaseEntity.java
@@ -1,12 +1,18 @@
 package artur.springframework.petclinic.domain;
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 }
