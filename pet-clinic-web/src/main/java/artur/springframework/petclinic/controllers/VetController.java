9cfaba69b472ed9e99fe1ca5e5c34d869fa963a8
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-11-26 14:06:06 -0300

    added json endpoint.

--- pet-clinic-web/src/main/java/artur/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/artur/springframework/petclinic/controllers/VetController.java
@@ -1,10 +1,14 @@
 package artur.springframework.petclinic.controllers;
 
+import artur.springframework.petclinic.domain.Vet;
 import artur.springframework.petclinic.services.VetServices;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +21,9 @@ public class VetController {
   public String listVets(Model model) {
     model.addAttribute("vets", vetServices.findAll());
     return "vets/index";
+  }
+  @GetMapping("/api/vets")
+  public @ResponseBody List<Vet> getVetsJson(){
+    return vetServices.findAll();
   }
 }
